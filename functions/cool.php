2a2b63609da45342c8f83a271b6dcdf5fc71bb46
Stefan Mars <stefanrmars@gmail.com>
2023-02-13 12:32:14 +0100

    niks

--- functions/cool.php
+++ functions/cool.php
@@ -1,9 +1,27 @@
 <?php
 function cool()
 {
+    $conn = mysqli_connect('rdbms.strato.de', 'dbu2577448', 'KabouterST06', 'dbs10041809');
+    if (!$conn) {
+        die('Connection Failed' . mysqli_connect_error());
+    }
+
+    $result = mysqli_query($conn, 'SELECT * FROM `posts`');
+
+    $collection = [];
+    if (mysqli_num_rows($result) > 0); {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $collection[] = $row;
+        }
+    }
     
     if (isset($_SESSION['status'])){
-        $rend = "jij bent officieel cool, want je bent ingelogd";
+        $rend = "<textarea></textarea><br>";
+        $rend .= "<button type='submit'>Post</button><br>";
+        foreach ($collection as $value) {
+            $rend .= "<table><tr>" . $value['Titel']."</tr>";
+            $rend .= "<tr>" . $value['Tekst'] . "</tr></table>";
+        }
     }
     else{
         $rend = "jij bent niet cool, want je bent niet ingelogd";
--- index.php
+++ index.php
@@ -15,6 +15,11 @@ $content = call_user_func($_GET['content']);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Stefan,Mars,PHP">
+    <meta name="description" content="PHP">
+    <meta name= "author" content="Stefan Mars">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="refresh" content="60">
     <style>
         <?php foreach (glob("*.css") as $filename) {
             include $filename;
@@ -28,12 +33,19 @@ $content = call_user_func($_GET['content']);
         <table class="navtb">
             <td class="navtd"><a class="link" href="index.php?content=Home">Home</a></td>
             <td class="navtd"><a class="link" href="index.php?content=Gallery">Gallery</a></td>
-            <td class="navtd"><a class="link" href="index.php?content=Login">Login</a></td>
             <td class="navtd"><a class="link" href="index.php?content=cool">Cool</a></td>
+            <?php
+            if (isset($_SESSION['status'])){
+                echo "<td class='navtd'><a class='link' href='index.php?content=Login'>Ingelogd</a></td>";
+            }
+            else{
+                echo "<td class='navtd'><a class='link' href='index.php?content=Login'>Inloggen</a></td>";
+            }
+            ?>
         </table>
     </nav>
     <?php
-    echo $content;
+        echo $content;
     ?>
     <script src="index.js"></script>
 </body>
