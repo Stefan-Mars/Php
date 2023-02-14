9d9e60481d5d23117bf19d45a193b039b4677b57
Stefan Mars <stefanrmars@gmail.com>
2023-02-14 10:59:48 +0100

    fix

--- functions/Home.php
+++ functions/Home.php
@@ -8,15 +8,16 @@ function Home()
     fclose($file);
         $homerend = '<div class="home">';
         $homerend .= '<h1>Welkom</h1>';
-        $homerend .= '<table><form action="" method="post">';
-        $homerend .= '<tr><input name="Antwoord1" placeholder="Getal 1"></input></tr>';
-        $homerend .= '<tr><input name="Antwoord2" placeholder="Getal 2"></input></tr>';
-        $homerend .= '<tr><button type="submit" id="submit">Submit</button></tr>';
-        $homerend .= '</form></table>';
-        if (isset($_POST["Antwoord1"]) && isset($_POST["Antwoord2"])) {
-            $antwoord = (int) $_POST["Antwoord1"] + (int) $_POST["Antwoord2"];
-            $homerend .= "het antwoord is $antwoord";
-        }
+        $homerend .= 'Login en maak daarna een post';
+        // $homerend .= '<table><form action="" method="post">';
+        // $homerend .= '<tr><input name="Antwoord1" placeholder="Getal 1"></input></tr>';
+        // $homerend .= '<tr><input name="Antwoord2" placeholder="Getal 2"></input></tr>';
+        // $homerend .= '<tr><button type="submit" id="submit">Submit</button></tr>';
+        // $homerend .= '</form></table>';
+        // if (isset($_POST["Antwoord1"]) && isset($_POST["Antwoord2"])) {
+        //     $antwoord = (int) $_POST["Antwoord1"] + (int) $_POST["Antwoord2"];
+        //     $homerend .= "het antwoord is $antwoord";
+        // }
         $homerend .= '<br>';
         $homerend .= '<br>';
         $homerend .= 'Dit staat er in mijn CSS:<br>';
@@ -24,6 +25,6 @@ function Home()
         $homerend .= $filecontent;
         $homerend .= '</code>';
         $homerend .= '</div>';
-        $homerend .= gallery();
+        // $homerend .= gallery();
         return $homerend;
 }
